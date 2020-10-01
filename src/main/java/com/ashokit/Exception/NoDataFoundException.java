22dfb27af2d04f013af39d87eadaa4de5a4ee501
sriharinayudu <sriharinayudut@xyz>
2020-10-01 18:32:25 +0530

    hai

--- src/main/java/com/ashokit/Exception/NoDataFoundException.java
+++ src/main/java/com/ashokit/Exception/NoDataFoundException.java
@@ -2,9 +2,7 @@ package com.ashokit.Exception;
 
 public class NoDataFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+//hai
 	private static final long serialVersionUID = 1L;
 
 	public NoDataFoundException(String msg)
